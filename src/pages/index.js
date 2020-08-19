444efb82e9914ebe75cede12075f5519734653b9
DwanW <kunvang@yahoo.com>
2020-08-18 23:08:23 -0600

    added skill section

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import styles from "./index.module.css";
 
 import Hero from '../sections/hero';
 import AboutMe from '../sections/about-me';
+import Skills from '../sections/skills'
 
 const IndexPage = () => (
     <Wrapper>
         <div className={`container ${styles.layout}`}>
             <Hero />
             <AboutMe />
+            <Skills />
         </div>
     </Wrapper>
 )
